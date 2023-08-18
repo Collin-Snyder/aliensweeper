13b974e1ff1da70a0938907b1b719aa3d87fa82b
Collin Snyder <collinsnyder@Collins-MacBook-Pro.local>
2023-08-17 22:24:23 -0600

    Try serving html directly

--- server/index.js
+++ server/index.js
@@ -2,16 +2,18 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const port = process.env.PORT || 3000;
+const html = require(path.join(__dirname + "/../client/dist/index.html"));
 
 const app = express();
 
-app.use(express.static(path.join(__dirname + "/../client/dist/")));
+// app.use(express.static(path.join(__dirname + "/../client/dist/")));
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("Welcome to your Express-React app!");
-})
+  // res.send("Welcome to your Express-React app!");
+  res.send(html);
+});
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}!`);
